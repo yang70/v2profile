29cd185402252d171100668ba239612bd6c1ef19
Matt Yang <yang70@gmail.com>
2016-04-27 22:05:17 -0700

    devise installed, removed sign up view/controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   resources :posts
+
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
